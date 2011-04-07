d9ca41f247d4728b84bd4f15257c23bda464793c
kopytek <tylkonieto1@gmail.com>
2011-04-07 04:07:01 +0200

    dodane brakujace pliki

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+    user.name                       'piotrek'
+    user.email                      'mail@mail.com'
+    user.password                   'foobar'
+    user.password_confirmation      'foobar'
+end  
