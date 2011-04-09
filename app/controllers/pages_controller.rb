dc28bd24cda164bc36e2961c67f57ea8a2feab7d
kopytek <tylkonieto1@gmail.com>
2011-04-09 18:12:14 +0200

    flash msg for new user, registration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
+  
+  def last
+      if User.count == 0 
+          'brak uzytkownikow w bazie'
+      else
+          User.last.name
+      end
+    end
+  
   def home
     @title = "home"
+    @users = User
+    @last = last
   end
 
   def contact
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
 
   def new
     @title = "Sign up"
+    @user = User.new
   end
+    
+    def create
+        @user = User.new(params[:user])
+        if @user.save
+            # udalo sie zapisac usera do bazy
+            flash[:success] = "Witaj " + @user.name 
+            redirect_to user_path(@user) 
+            # rownie dobrze redirect_to @user bedzie dzialac
+        else
+            @title = 'Sign up'  
+            render 'new'
+        end
+    end
 
 end
